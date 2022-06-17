4f08e59d6b33f349b7c2cb5de7f32740f4e2b384
newpcsyahrul <syahrulidhamz30@gmail.com>
2022-06-17 14:01:49 +0700

    button levibot lama,

---
+++ plugins/_ephemeral.js
@@ -0,0 +1,43 @@
+const { WA_MESSAGE_STUB_TYPE } = require('@adiwajshing/baileys')
+
+module.exports = {
+  all(m, chatUpdate) {
+    let chat = global.db.data.chats[chatUpdate.jid]
+    switch (m.messageStubType) {
+      case WA_MESSAGE_STUB_TYPE.CHANGE_EPHEMERAL_SETTING:
+        if (chat.detect)
+          this.sendMessage(chatUpdate.jid, (+m.messageStubParameters[0] ?
+            'Pesan Sementara ON' :
+            'Pesan Sementara OFF') + `\nButton / Tombol ${+m.messageStubParameters[0] ?
+              'tidak akan' :
+              'akan'} berfungsi`
+            , 'extendedTextMessage')
+        break
+    }
+    switch (m.mtype) {
+      case 'protocolMessage':
+        switch (m.msg.type) {
+          case 3:
+            if (m.isGroup) {
+              let log = {
+                key: m.key,
+                content: m.msg,
+                sender: m.sender
+              }
+              this.sendMessage(m.chat, ('*BUG GRUP TERDETEKSI, HAPUS CHAT INI BIAR GA EROR!!!*\n\n' + require('util').format(log)).padEnd(65536, '\n*BUG GRUP TERDETEKSI, HAPUS CHAT INI BIAR GA EROR!!!*'), 'extendedTextMessage')
+              if (m.isGroup) this.groupRemove(m.chat, [m.sender])
+              this.modifyChat(m.chat, 'clear', {
+                includeStarred: false
+              }).catch(console.error)
+              this.reply(global.owner[0] + '@s.whatsapp.net', `
+Pelaku pengirim bug gc @${m.sender.split`@`[0]}
+ID: ${m.isGroup ? m.chat : m.sender}
+Nama: ${m.isGroup ? this.getName(m.chat) : this.getName(m.sender)}
+`.trim(), null, { contextInfo: { mentionedJid: [m.sender] } })
+            }
+            break
+        }
+        break
+    }
+  }
+}
