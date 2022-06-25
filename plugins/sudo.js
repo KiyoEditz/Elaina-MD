eb2cebda08d667973bb746fc47d9198e2b37545d
rdp4ever <syahrulidhamz30@gmail.com>
2022-06-25 12:02:14 +0700

    sudo

--- plugins/sudo.js
+++ plugins/sudo.js
@@ -1,4 +1,8 @@
-const { newMessagesDB } = require("@adiwajshing/baileys")
+const {
+  proto,
+  generateWAMessage,
+  areJidsSameUser
+} = require('@adiwajshing/baileys')
 
 let handler = async (m, { conn, text }) => {
   if (!text) throw false
@@ -7,11 +11,21 @@ let handler = async (m, { conn, text }) => {
   else who = m.chat
   if (!who) throw 'Tag seseorang'
   let txt = text.replace('@' + who.split`@`[0], '').trimStart()
-  conn.emit('chat-update', {
-    jid: who,
-    hasNewMessage: true,
-    messages: newMessagesDB([conn.cMod(m.chat, m, txt, who)])
+
+  let messages = await generateWAMessage(m.chat, { text: txt, mentions: m.mentionedJid }, {
+    userJid: who,
+    quoted: m.quoted && m.quoted.fakeObj
   })
+  messages.key.fromMe = areJidsSameUser(who, conn.user.id)
+  messages.key.id = m.key.id
+  messages.pushName = m.name
+  if (m.isGroup)
+    messages.key.participant = messages.participant = who
+  let msg = {
+    messages: [proto.WebMessageInfo.fromObject(messages)].map(v => (v.conn = this, v)),
+    type: 'append'
+  }
+  conn.ev.emit('messages.upsert', msg)
 }
 handler.command = /^sudo$/
 handler.rowner = true
