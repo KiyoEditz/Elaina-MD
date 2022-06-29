777aad3ea31bc75fca6f974ef38f721f8372c1a3
rdp4ever <syahrulidhamz30@gmail.com>
2022-06-29 22:59:15 +0700

    photoxy, textpro fix

--- plugins/photooxy.js
+++ plugins/photooxy.js
@@ -3,16 +3,31 @@ let handler = async (m, { conn, text, usedPrefix, command }) => {
   let [t1, t2] = text.split`|`
   if (!t1) throw `Ketik ${usedPrefix + command} teks`
   let url
-  url = await fetch(global.API('lolhuman', `/api/photooxy1/${command}`, { text: t1 }, 'apikey')).catch(e => {
-    if (!t1 && !t2) throw `_Masukkan teks 2_\n\nKetik ${usedPrefix + command} teks | teks2`
-    url = await fetch(global.API('lolhuman', `/api/photooxy1/${command}`, { text1: t1, text2: t2 }, 'apikey'))
-  })
-  if (!url.ok) throw 'Server Error.. Harap lapor owner'
+  url = await fetch(global.API('lolhuman', `/api/photooxy1/${command}`, { text: t1 }, 'apikey'))
+  if (!url.ok) {
+    if (!t2) throw `_Masukkan teks 2_\n\nKetik ${usedPrefix + command} teks | teks2`
+    url = fetch(global.API('lolhuman', `/api/photooxy2/${command}`, { text1: t1, text2: t2 }, 'apikey'))
+    if (!url.ok) throw `Server Error`
+  }
   await conn.sendFile(m.chat, await url.buffer(), 'photooxy.jpg', `${command}`, m)
 }
-handler.command = handler.help = ["Shadow", "Cup", "Cup2", "Romance", "Smoke", "BurnPaper", "LoveMessage", "UnderGrass", "Love", "Coffe", "WoodHeart", "WoodenBoard", "Summer3D", "WolfMetal", "Nature3D", "UnderWater", "GoldenRose", "SummerNature", "FallLeaves", "Flamming", "HarryPotter", "CarvedWood", "Tiktok", "Arcade8bit", "Battlefield4", "PUBG", "BannerLOL"].sort((a, b) => a - b)
+handler.command = handler.help = [
+  'shadow', 'cup',
+  'cup2', 'romance',
+  'smoke', 'burnpaper',
+  'lovemessage', 'undergrass',
+  'love', 'coffe',
+  'woodheart', 'woodenboard',
+  'summer3d', 'wolfmetal',
+  'nature3d', 'underwater',
+  'goldenrose', 'summernature',
+  'fallleaves', 'flamming',
+  'harrypotter', 'carvedwood',
+  'tiktok', 'arcade8bit',
+  'battlefield4', 'pubg',
+  'bannerlol'
+].sort((a, b) => a - b)
 handler.tags = ['photooxy']
 handler.limit = true
 
 module.exports = handler
-
--- plugins/textpro.js
+++ plugins/textpro.js
@@ -3,16 +3,34 @@ let handler = async (m, { conn, text, usedPrefix, command }) => {
   let [t1, t2] = text.split`|`
   if (!t1) throw `Ketik ${usedPrefix + command} teks`
   let url
-  url = await fetch(global.API('lolhuman', `/api/textprome/${command}`, { text: t1 }, 'apikey')).catch(e => {
-    if (!t1 && !t2) throw `_Masukkan teks 2_\n\nKetik ${usedPrefix + command} teks | teks2`
-    url = await fetch(global.API('lolhuman', `/api/textprome/${command}`, { text1: t1, text2: t2 }, 'apikey'))
-  })
-  if (!url.ok) throw 'Server Error.. Harap lapor owner'
+  url = await fetch(global.API('lolhuman', `/api/textprome/${command}`, { text: t1 }, 'apikey'))
+  if (!url.ok) {
+    if (!t2) throw `_Masukkan teks 2_\n\nKetik ${usedPrefix + command} teks | teks2`
+    url = await fetch(global.API('lolhuman', `/api/textprome2/${command}`, { text1: t1, text2: t2 }, 'apikey'))
+    if (!url.ok) throw `Server Error`
+  }
   await conn.sendFile(m.chat, await url.buffer(), 'textpro.jpg', `*TEXTPRO*\n*Effect:* ${command}`, m)
+
 }
 handler.tags = ['textpro']
 handler.limit = true
-handler.command = handler.help = ["Shadow", "Cup", "Cup2", "Romance", "Smoke", "BurnPaper", "LoveMessage", "UnderGrass", "Love", "Coffe", "WoodHeart", "WoodenBoard", "Summer3D", "WolfMetal", "Nature3D", "UnderWater", "GoldenRose", "SummerNature", "FallLeaves", "Flamming", "HarryPotter", "CarvedWood", "Tiktok", "Arcade8bit", "Battlefield4", "PUBG", "BannerLOL"].sort((a, b) => a - b)
+handler.command = handler.help = [
+  'blackpink', 'neon', 'greenneon',
+  'futureneon', 'sandwriting', 'sandsummer',
+  'sandengraved', 'metaldark', 'neonlight',
+  'holographic', 'text1917', 'minion',
+  'deluxesilver', 'newyearcard', 'bloodfrosted',
+  'halloween', 'jokerlogo', 'fireworksparkle',
+  'natureleaves', 'bokeh', 'toxic',
+  'strawberry', 'box3d', 'roadwarning',
+  'breakwall', 'icecold', 'luxury',
+  'cloud', 'summersand', 'horrorblood',
+  'thunder', 'magma', 'impressiveglitch',
+  'harrypotter', 'foggywindow', 'watercolor',
+  'wonderfulgraffiti', 'pornhub', 'glitch',
+  'avenger', 'space', 'ninjalogo',
+  'marvelstudio', 'lionlogo', 'wolflogo',
+  'steel3d', 'wallgravity', 'coolgravity'
+].sort((a, b) => a - b)
 
 module.exports = handler
-
