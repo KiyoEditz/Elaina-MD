506b9e2cc12a6c193652a491f6269c0400a7b8c7
syahrulpanel <syahrulidhamz30@gmail.com>
2023-02-13 22:19:53 +0000

    ig try

--- plugins/ig.js
+++ plugins/ig.js
@@ -1,11 +1,24 @@
-const { instagramdl, instagramdlv2, instagramdlv3, instagramdlv4 , savefrom } = require('@bochilteam/scraper')
+const { savefrom } = require('@bochilteam/scraper')
 let handler = async (m, { conn, args, usedPrefix, command }) => {
 
   if (!args[0]) throw `_Masukkan link_\n\nContoh:\n${usedPrefix + command} https://www.instagram.com/p/linkurl`
   if (/stories/i.test(args[0])) throw `_Perintah ini untuk mendownload post IG, bukan story_\nGunakan fitur berikut \n\n${usedPrefix}igstory username`
   if (!args[0].match(/(https:\/\/www.)?instagram.com\/([A-Za-z0-9.\_]*\/)?(reel|p|tv)/)) throw `Link tidak valid \n\nContoh:\n${usedPrefix + command} https://www.instagram.com/p/linkurl`
   let dl = await savefrom(args[0])
-    .catch(e => m.reply('Error'))
+    .catch(async e => {
+      await global.API('vhtear', '/instadl' , { link: args[0]}, 'apikey').then(res => (
+        {
+  url: res.result.post.map(v => ({
+    url: v.urlDownload
+  })
+  ),
+  meta: {
+    title: res.result.caption
+  }
+}
+))
+      }
+      ).catch(e => m.reply('Error tidak diketahui..'))
   await m.reply('_Sedang proses mengirim..._')
 
   for (let { url } of dl.url) {
@@ -17,5 +30,3 @@ handler.help = ['ig', 'instagram'].map(v => v + ' <link>')
 handler.tags = ['downloadersosmed']
 handler.command = /^(ig|instagram)2?$/i
 handler.limit = true
-
-module.exports = handler
