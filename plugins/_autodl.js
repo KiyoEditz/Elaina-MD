45e81090c107653d428069dbf9739f4e34983d10
syahrulpc <syahrulidhamz30@gmail.com>
2023-02-17 13:15:23 +0700

    lupa m.text nya

--- plugins/_autodl.js
+++ plugins/_autodl.js
@@ -48,9 +48,9 @@ handler.before = async function (m, { isPrems, match }) {
         }
         let link = (/https?:\/\/(www\.)?instagram\.com\/(p|reel|tv)\/.*/i.exec(m.text))[0].split(/\n| /i)[0]
         m.reply(acc)
-        let dl = await savefrom(args[0])
+        let dl = await savefrom(m.text)
             .catch(async e => {
-                let res = await fetch(global.API('vhtear', '/instadl', { link: args[0] }, 'apikey'))
+                let res = await fetch(global.API('vhtear', '/instadl', { link: m.text }, 'apikey'))
                 let json = await res.json()
                 let obj = {
                     url: json.result.post.map(v => ({
