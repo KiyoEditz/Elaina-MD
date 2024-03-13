c85bcc2fa9c2f3155cc9fa0af5fd1c7d64d1305d
syahrul <syahrulidhamz30@gmail.com>
2024-03-13 18:41:03 +0700

    tes community

--- plugins/skata.js
+++ plugins/skata.js
@@ -22,6 +22,7 @@ Ariffb
 Syahrul`.trim()
 let poin = 500
 let handler = async (m, { conn, text, isPrems, isROwner, usedPrefix, command }) => {
+	if (m.chat !== '120363242705186427@g.us') throw `Group ini tidak support Game Sambung Kata, silahkan pindah ke group yang ada di komunitas`
 	let isDebug = /debug/i.test(command) && isROwner
 	//if (!isPrems) throw `Game ini dalam tahap pengemmbangan.. cooming soon`
 	conn.skata = conn.skata ? conn.skata : {}
