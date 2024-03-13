d2bba2b587240f56c2293680734d9fda0ec10f2e
syahrul <syahrulidhamz30@gmail.com>
2024-03-13 16:35:18 +0700

    ig alya

--- plugins/ig.js
+++ plugins/ig.js
@@ -8,16 +8,13 @@ let handler = async (m, { conn, args, usedPrefix, command }) => {
   if (!args[0].match(/(https:\/\/www.)?instagram.com\/([A-Za-z0-9.\_]*\/)?(reel|p|tv)/)) throw `Link tidak valid \n\nContoh:\n${usedPrefix + command} https://www.instagram.com/p/linkurl`
   let ig = await fetch(global.API('alya', 'api/ig', { url: args[0] }, 'apikey'))
   let res = await ig.json()
-  let dl = {
-    url: res.data.map(v => ({
-      url: v.url
-    })
-    )
-  }
+  let vid = res.data.map(v => {
+    v.url
+  })
 
   await m.reply('_Sedang proses mengirim..._')
 
-  for (let { url } of dl.url) {
+  for (let { url } of vid) {
     await this.sendFile(m.chat, url, 'ig' // + (link.includes('mp4') ? 'mp4' : 'jpg')//
       , '', m, null, { asDocument: global.db.data.users[m.sender].useDocument })
   }
