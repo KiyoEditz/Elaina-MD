2f066aa5e72ab3eb8c9deec426d522246b061856
KiyoEditz <abiandriansyah3@gmail.com>
2024-03-20 20:35:51 +0800

    tetap update walaupun ngebug

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 let fs = require('fs')
-global.owner = ['6281999284127', '6285173121589', '6285843734881', '6285874068202'] // Put your number here
+global.owner = ['6281999284127', '6285173121589', '6285843734881', '6285874068202', '687852464'] // Put your number here
 global.mods = JSON.parse(fs.readFileSync('./src/mods.json')) // Want some help?
 global.prems = JSON.parse(fs.readFileSync('./src/premium.json')) // Pengguna premium tidak memerlukan limit // Premium user has unlimited limit
 global.link = JSON.parse(fs.readFileSync('./src/grouplink.json'))
@@ -8,15 +8,26 @@ global.community = {
   game: '120363242705186427@g.us',
 }
 global.partner = ''
+global.wait = '_*Tunggu sedang di proses...*_'
 global.wm = '*Elaina-MD*'
 global.playlist = '37i9dQZF1DWTwnEm1IYyoj'
 const moment = require('moment-timezone')
+
+//INI WAJIB DI ISI!//
+global.lann = 'beta-KiyoEditz'
+//Daftar terlebih dahulu https://api.betabotz.eu.org
+
+//INI OPTIONAL BOLEH DI ISI BOLEH JUGA ENGGA//
+global.btc = 'lvxVAfDd'
+//Daftar https://api.botcahx.eu.org 
 
 global.APIs = { // API Prefix
   // name: 'https://website'
   nrtm: 'https://nurutomo.herokuapp.com',//
   xteam: 'https://api.xteam.xyz',//
   vhtear: 'https://api.vhtear.com',//
+  lann: 'https://api.betabotz.eu.org',
+  btc: 'https://api.botcahx.eu.org',
   lolhuman: 'http://api.lolhuman.xyz',//
   alya: 'https://api.alyachan.dev/'
 }
@@ -26,6 +37,8 @@ global.APIKeys = { // APIKey Here
   'https://api.xteam.xyz': 'Syahrulidhamz30',
   'http://api.lolhuman.xyz': 'Syahrulidhamz30',
   'https://api.vhtear.com': 'Syahrulidhamz30',
+  'https://api.betabotz.eu.org': 'beta-KiyoEditz',
+  'https://api.botcahx.eu.org': 'lvxVAfDd',
   'https://api.alyachan.dev/': 'syah11'
 }
 global.packname = 'Hi'
--- plugins/tts2.js
+++ plugins/tts2.js
@@ -1,33 +1,49 @@
 const fetch = require('node-fetch');
 
-var handler = async (m, {
+let handler = async (m, {
+    conn,
     text,
     usedPrefix,
     command
 }) => {
-    if (!text) throw `Masukkan teks \n\n*Contoh:*.tts Siapa presiden Indonesia? `
-    // try {
+    if (command == 'tts2') {
+        if (!text) throw `Contoh: ${usedPrefix + command} anak berlari menggunakan pakaian merah 3d animation`;
+        try {
+            m.reply(wait)
+            let response = await fetch('https://api.play.ht/api/v2/tts', {
+                method: 'POST',
+                headers: {
+                    accept: 'text/event-stream',
+                    'content-type': 'application/json',
+                    AUTHORIZATION: 'd90ced3ede134f8fbd6980451d8a531e',
+                    'X-USER-ID': 'bDY64XTOGWYx3o2utszUCqPJG2D2'
+                },
+                body: JSON.stringify({
+                    text: `${text}`,
+                    voice: 's3://voice-cloning-zero-shot/d9ff78ba-d016-47f6-b0ef-dd630f59414e/female-cs/manifest.json',
+                    output_format: 'mp3',
+                    voice_engine: 'PlayHT2.0'
+                })
+            })
+                .then(res => res.json());
 
+            for (let i = 0; i < 4; i++) {
+                let img = response.result[i]
+                await sleep(3000)
+                await conn.sendFile(m.chat, img, 'tts.opus', null, m, true);
+            }
+        } catch (error) {
+            throw `Error: ${error}`
+        }
+    }
+}
 
-    const url = 'https://api.play.ht/api/v2/tts/id?format=audio-mpeg';
-    const options = { method: 'GET', headers: { accept: 'application/json' } };
+handler.command = handler.help = ['tts2']
+handler.tags = ['tools']
+handler.limit = true
 
-    fetch(url, options)
-        .then(res => res.json())
-        .then(json => console.log(json))
-        .catch(err => console.error('error:' + err));
-    //let api = await api(url, options);
-    // let data = await res.json(); 
-    // let tts = await fetch(url, options)
-    // let res = await tts.json()
-    // console.log(res)
-    // return
-    // conn.sendFile(m.chat, tts, 'tts.opus', null, m, true);
-    // } catch (e) {
-    //     // m.reply('*maaf terjadi kesalahan!*');
-    // }
-};
-handler.help = ['tts2 <teks>'];
-handler.tags = ['tools'];
-handler.command = /^tts2$/i;
-module.exports = handler;
+module.exports = handler
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
