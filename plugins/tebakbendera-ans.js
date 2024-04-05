1d6f2ad5ff31b5438fe3cb717b94cdd9cf80e4ad
Kiyo Editz <118544063+KiyoEditz@users.noreply.github.com>
2024-04-05 09:58:50 +0700

    Fix tebakbendera-ans.js

--- plugins/tebakbendera-ans.js
+++ plugins/tebakbendera-ans.js
@@ -1,3 +1,5 @@
+const similarity = require('similarity')
+const threshold = 0.72
 let handler = m => m
 handler.before = async function (m) {
   let id = m.chat
@@ -5,7 +7,7 @@ handler.before = async function (m) {
   conn.tebakbendera = conn.tebakbendera ? conn.tebakbendera : {}
   if (!(id in conn.tebakbendera)) return m.reply('Soal itu telah berakhir')
   if (m.quoted.id == conn.tebakbendera[id][0].id) {
-    let json = JSON.parse(JSON.stringify(conn.tebakbendera[id][1]))
+    /*let json = JSON.parse(JSON.stringify(conn.tebakbendera[id][1]))
     // m.reply(JSON.stringify(json, null, '\t'))
     if (m.text.toLowerCase() == json.jawaban.toLowerCase()) {
       global.DATABASE._data.users[m.sender].uang += conn.tebakbendera[id][2]
@@ -16,6 +18,19 @@ handler.before = async function (m) {
     else m.reply(`*Salah!*`)
   }
   return !0
+}*/
+let json = JSON.parse(JSON.stringify(this.tebakbendera[id][1]))
+        // m.reply(JSON.stringify(json, null, '\t'))
+        if (m.text.toLowerCase() == json.jawaban.toLowerCase().replace(' ', '')) {
+            global.db.data.users[m.sender].exp += this.tebakbendera[id][2]
+            this.reply(m.chat, `*Benar!*\n+${this.tebakbendera[id][2]} XP`, m)
+            this.sendMessage(m.chat, { delete: this.tebakbendera[id][0].key }).catch(e => e)
+            clearTimeout(this.tebakbendera[id][3])
+            delete this.tebakbendera[id]
+        } else if (similarity(m.text.toLowerCase(), json.jawaban.toLowerCase().trim()) >= threshold) m.reply(`*Dikit Lagi!*`)
+        else m.reply(`*Salah!*`)
+    }
+    return !0
 }
 handler.exp = 0
 
