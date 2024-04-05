ba6ddd759d03f761ae3a490f2cec5785bda9e5df
Kiyo Editz <118544063+KiyoEditz@users.noreply.github.com>
2024-04-05 20:50:12 +0700

    update plugins/update.js

--- plugins/update.js
+++ plugins/update.js
@@ -1,3 +1,30 @@
+let { execSync } = require('child_process')
+let handler = async (m, { conn, text, isROwner }) => {
+  if (global.conn.user.jid == conn.user.jid) {
+    let stdout = execSync('git remote set-url origin https://github.com/ilmanhdyt/ShiraoriBOT.git && git pull' + (isROwner && text ? ' ' + text : ''))
+    if (isROwner) require('fs').readdirSync('plugins').map(v => global.reload('', v))
+    m.reply(stdout.toString())
+  }
+}
+/*
+handler.help = ['update']
+handler.tags = ['host']
+handler.command = /^update$/i
+handler.owner = true
+handler.mods = false
+handler.premium = false
+handler.group = false
+handler.private = false
+
+handler.admin = false
+handler.botAdmin = false
+
+handler.fail = null
+handler.exp = 0
+
+module.exports = handler
+*/
+/*
 const { default: fetch } = require('node-fetch')
 const { createWriteStream, existsSync } = require('fs')
 const { promisify } = require('util')
@@ -46,8 +73,8 @@ handler.all = async m => {
         return !0
     }
 }
-
-handler.rowner = true
+*/
+//handler.rowner = true
 handler.help = ['update']
 handler.tags = ['owner']
 handler.command = ['update']
