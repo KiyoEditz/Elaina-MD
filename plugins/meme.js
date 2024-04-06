80957ea0d2776cdfcb1268857655650fd4b0393d
Kiyo Editz <118544063+KiyoEditz@users.noreply.github.com>
2024-04-06 10:00:14 +0700

    update plugins/meme.js

--- plugins/meme.js
+++ plugins/meme.js
@@ -1,7 +1,7 @@
 let fetch = require('node-fetch')
 let handler = async (m, { conn, usedPrefix, command }) => {
   //let res = global.API('lolhuman', '/api/meme/memeindo', '', 'apikey')
-  let res = await fetch(`https://api.botcahx.eu.org/api/random/meme?apikey=${btc}`).then(result => result.buffer())
+  let res = await fetch(`https://api.botcahx.eu.org/api/random/meme?apikey=lvxVAfDd`).then(result => result.buffer())
   m.reply('_Sedang mencari..._')
   await conn.sendFile(m.chat, res, 'img.jpg', `(ﾉ◕ヮ◕)ﾉ*.✧`, m)
 }
