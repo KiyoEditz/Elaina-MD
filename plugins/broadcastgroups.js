adec594106efbca45fe4eb5cf44413bc7a812c37
Kiyo Editz <118544063+KiyoEditz@users.noreply.github.com>
2024-05-03 16:08:06 +0700

    Fix

--- plugins/broadcastgroups.js
+++ plugins/broadcastgroups.js
@@ -1,24 +1,22 @@
+
+
 let handler = async (m, { conn, text }) => {
-  let groups = conn.chats.all().filter(v => v.jid.endsWith('g.us') && !v.read_only && v.message && !v.announce).map(v => v.jid)
+  let groups = Object.entries(conn.chats).filter(([jid, chat]) => jid.endsWith('@g.us') && chat.isChats && !chat.metadata?.read_only && !chat.metadata?.announce).map(v => v[0])
   let cc = text ? m : m.quoted ? await m.getQuotedObj() : false || m
   let teks = text ? text : cc.text
-  let content = conn.cMod(m.chat, cc, /bc|broadcast/i.test(teks) ? teks : teks + '\n' + conn.readmore + '「 All Group Broadcast 」')
-  for (let id of groups) conn.copyNForward(id, content, true)
   conn.reply(m.chat, `_Mengirim pesan broadcast ke ${groups.length} grup_`, m)
+  for (let id of groups) await conn.copyNForward(id, conn.cMod(m.chat, cc, /bc|broadcast/i.test(teks) ? teks : teks + '\n' + readMore + set.footer + ' \n' + randomID(32)), true).catch(_ => _)
+  m.reply(`Selesai Broadcast ${groups.length} Group`)
 }
-handler.help = ['broadcastgroup <teks>']
+handler.help = ['broadcastgroup', 'bcgc'].map(v => v + ' <teks>')
 handler.tags = ['owner']
 handler.command = /^(broadcast|bc)(group|grup|gc)$/i
+
 handler.owner = true
-handler.mods = false
-handler.premium = false
-handler.group = false
-handler.private = false
-
-handler.admin = false
-handler.botAdmin = false
-
-handler.fail = null
 
 module.exports = handler
 
+const more = String.fromCharCode(8206)
+const readMore = more.repeat(4001)
+
+const randomID = length => require('crypto').randomBytes(Math.ceil(length * .5)).toString('hex').slice(0, length)
