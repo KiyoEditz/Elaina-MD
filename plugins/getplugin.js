3bb2f45bf03f2e8b8b669f97a3ee45383d845fac
Kiyo Editz <118544063+KiyoEditz@users.noreply.github.com>
2024-05-06 09:41:43 +0700

    Update getplugin.js

--- plugins/getplugin.js
+++ plugins/getplugin.js
@@ -1,3 +1,69 @@
+const fs = require('fs')
+const syntaxError =require('syntax-error')
+const path =require('path')
+
+const _fs = fs.promises
+
+let handler = async (m, { text, usedPrefix, command, __dirname }) => {
+    if (!text) throw `
+✳️ user  : ${usedPrefix + command} <name file>
+
+📌 Example:
+        ${usedPrefix}getfile main.js
+        ${usedPrefix}getplugin owner
+`.trim()
+    if (/p(lugin)?/i.test(command)) {
+        const filename = text.replace(/plugin(s)\//i, '') + (/\.js$/i.test(text) ? '' : '.js')
+        const pathFile = path.join(__dirname, filename)
+        const file = await _fs.readFile(pathFile, 'utf8')
+        m.reply(file)
+        const error = syntaxError(file, filename, {
+            sourceType: 'module',
+            allowReturnOutsideFunction: true,
+            allowAwaitOutsideFunction: true
+        })
+        if (error) {
+            await m.reply(`
+❎ bug found in  *${filename}*:
+
+${error}
+
+`.trim())
+        }
+
+    } else {
+        const isJavascript = /\.js/.test(text)
+        if (isJavascript) {
+            const file = await _fs.readFile(text, 'utf8')
+            m.reply(file)
+            const error = syntaxError(file, text, {
+                sourceType: 'module',
+                allowReturnOutsideFunction: true,
+                allowAwaitOutsideFunction: true
+            })
+            if (error) {
+                await m.reply(`
+❎ bug found in *${text}*:
+
+${error}
+
+`.trim())
+            }
+        } else {
+            const file = await _fs.readFile(text, 'base64')
+            await m.reply(Buffer.from(file, 'base64'))
+        }
+    }
+}
+handler.help = ['plugin', 'file'].map(v => `get${v} <name file>`)
+handler.tags = ['owner']
+handler.command = /^g(et)?(p(lugin)?|f(ile)?)$/i
+
+handler.owner = true
+
+module.exports = handler
+    
+/*
 let cp = require('child_process')
 let { promisify } = require('util')
 let exec = promisify(cp.exec).bind(cp)
@@ -24,3 +90,4 @@ handler.tags = ['owner']
 handler.command = /^(getplugin|gp)$/i
 
 module.exports = handler
+*/
