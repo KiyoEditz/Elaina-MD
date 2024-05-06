c50bb3644b386fb97eb98b57e6946cc687f9108c
Kiyo Editz <118544063+KiyoEditz@users.noreply.github.com>
2024-05-06 09:29:36 +0700

    Update broadcast.js

--- plugins/broadcast.js
+++ plugins/broadcast.js
@@ -1,3 +1,29 @@
+
+
+var crypto = require('crypto')
+
+let handler = async (m, { conn, text }) => {
+  let chats = Object.entries(conn.chats).filter(([_, chat]) => chat.isChats).map(v => v[0])
+  let cc = conn.serializeM(text ? m : m.quoted ? await m.getQuotedObj() : false || m)
+  let teks = text ? text : cc.text
+  conn.reply(m.chat, `_Mengirim pesan broadcast ke ${chats.length} chat_`, m)
+  for (let id of chats) await conn.copyNForward(id, conn.cMod(m.chat, cc, /bc|broadcast/i.test(teks) ? teks : teks + '\n' + readMore + set.footer + randomID(32)), true).catch(_ => _)
+  m.reply(`_Selesai broadcast ${chats.length} chat!_`)
+}
+handler.help = ['broadcast', 'bc'].map(v => v + ' <teks>')
+handler.tags = ['owner']
+handler.command = /^(broadcast|bc|bcs)$/i
+
+handler.owner = true
+
+module.exports = handler
+
+const more = String.fromCharCode(8206)
+const readMore = more.repeat(4001)
+
+const randomID = length => require('crypto').randomBytes(Math.ceil(length * .5)).toString('hex').slice(0, length)
+
+
 /*let handler = async (m, { conn, text, command }) => {
   let chats = conn.chats.all().filter(v => !v.read_only && v.message).map(v => v.jid)
   if (/pc/i.test(command)) chats = conn.chats.all().filter(v => !v.read_only && v.message && !v.jid.endsWith('g.us')).map(v => v.jid)
