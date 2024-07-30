3cc0c726b5f7b2b31cfa286f51d58939345f9ba4
KiyoEditz <abiandriansyah3@gmail.com>
2024-07-30 10:20:20 +0700

    add fitur toptv

---
+++ plugins/toptv.js
@@ -0,0 +1,32 @@
+let { generateWAMessageContent } = require("@whiskeysockets/baileys");
+
+let handler = async (m, { conn, usedPrefix, command }) => {
+  let q = m.quoted ? m.quoted : m;
+  let mime = (m.quoted ? m.quoted : m.msg).mimetype || "";
+  if (!/webp|image|video|gif|viewOnce/g.test(mime))
+    return m.reply(
+      `*• Example :* ${usedPrefix + command} *[reply/send media]*`,
+    );
+  let media = await q.download();
+  let msg = await generateWAMessageContent(
+    {
+      video: media,
+    },
+    {
+      upload: conn.waUploadToServer,
+    },
+  );
+  await conn.relayMessage(
+    m.chat,
+    {
+      ptvMessage: msg.videoMessage,
+    },
+    {
+      quoted: m,
+    },
+  );
+};
+handler.help = ["toptv [reply/send media]"];
+handler.tags = ["tools"];
+handler.command = ["toptv"];
+module.exports = handler;
