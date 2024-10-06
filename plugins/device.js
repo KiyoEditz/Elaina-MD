19c7c86bc90e9a47d81232653856181af6dcba23
KiyoEditz <abiandriansyah3@gmail.com>
2024-10-06 10:13:35 +0700

    it's a nice one

---
+++ plugins/device.js
@@ -0,0 +1,12 @@
+const { getDevice } = require('@adiwajshing/baileys')
+
+let handler = async (m) => {
+	m.reply(await getDevice(m.quoted ? m.quoted.id : m.key.id))
+}
+
+handler.help = ['device']
+handler.tags = ['tools']
+handler.command = /^(device)$/i
+handler.group = true
+
+module.exports = handler
